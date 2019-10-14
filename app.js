d5b5bc7c364fda4918078e5894bdc7fd113bbbcc
Matthew Huynh <mattrhuynh@gmail.com>
2019-10-14 16:00:55 -0400

    Front end stuff

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const routes = require('./routes/index');
+const bodyParser = require('body-parser');
 
 const app = express();
 
@@ -8,7 +9,9 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-app.use('/', routes);
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(express.static('public'));
+app.use('/', routes); // We will define all our routes in index.js
 
 // We'll be using the same Express app from start.js, index.js,.. to access our endpoints
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,38 @@
 const express = require('express');
+var data = require('../data/calendar.json')
 
 const router = express.Router();
 
 // Renders the 'form.pug' template from views
 router.get('/', (req, res) => {
-  res.render('form', {title: 'UTM Shuttle Bus Scheduler'});
+  res.render('form', {
+      title: 'UTM Shuttle Bus Scheduler', 
+      Months: data['Months'],
+      Years: data['Years']
+    });
 });
 
 // Handles the POST route from clicking on 'Submit'
-router.post('/', (req, res) => {
-    res.render('form', { title: 'Time Submission' });
-});
+// router.post('/', (req, res) => {
+//     console.log(req.body);
+//     res.render('form', { 
+//         title: 'Time Submission', 
+//         Months: data['Months']
+//     });
+// });
+
+
+//Handles the POST route from selecting a date from dropdown
+router.post("/dateChosen" , function(req, res){
+  console.log(req.body)
+  res.render('form', {
+      title: 'Date Chosen',
+      currentDate: 'The current date is ' + req.body.monthChosen,
+      locationChosen: 'The location is ' + req.body.busStop,
+      Months: data['Months'],
+      Years: data['Years']
+  })
+})
+
 
 module.exports = router;
